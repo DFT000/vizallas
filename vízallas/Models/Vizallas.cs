38cc953e7a088b2d434a936590931630c8e671fc
szkibak.szabolcs <szkibak.szabolcs@BURG-2E05A-02>
2025-09-30 13:08:16 +0200

    1. feladat kesz

---
+++ vízallas/Models/Vizallas.cs
@@ -0,0 +1,11 @@
+﻿namespace vízallas.Models
+{
+    public class Vizallas
+    {
+        public int Id { get; set; }
+        public string Folyó { get; set; } = string.Empty;
+        public string Település { get; set; } = string.Empty;
+        public DateTime Datum { get; set; }
+        public int VizallasErtek { get; set; }
+    }
+}
